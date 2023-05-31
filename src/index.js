fbb846abcfa42a1070a48bd4a4e8cbf3f0656a1b
Dangeagle <tenanhladang1506@gmail.com>
2023-05-31 11:16:49 +0700

    config setHeader on file main

--- src/index.js
+++ src/index.js
@@ -29,9 +29,28 @@ connectDB();
 app.use("/authen", authenRouter);
 app.use("/api", carzRouter);
 app.use(compression()); // Compress all routes
+const whitelist = ["*"];
 
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
 app.get("*", (req, res) => {
   res.send("404");
 });
