272d869e92534fb009847897efb5b1e1a1649580
Dangeagle <tenanhladang1506@gmail.com>
2023-05-31 11:24:38 +0700

    config vercel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var carzRouter = require("../src/router/carz.router");
 const mongoose = require("mongoose");
 const port = 3000;
 var config = require("../config");
-const compression = require("compression");
+// const compression = require("compression");
 const path = require("path");
 var dbUrl = config.dbUrl;
 
@@ -28,29 +28,32 @@ connectDB();
 
 app.use("/authen", authenRouter);
 app.use("/api", carzRouter);
-app.use(compression()); // Compress all routes
-const whitelist = ["*"];
+// app.use(compression()); // Compress all routes
+// const whitelist = ["*"];
 
-app.use((req, res, next) => {
-  const origin = req.get("referer");
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,Content-Type,Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === "OPTIONS") res.sendStatus(200);
-  else next();
+// app.use((req, res, next) => {
+//   const origin = req.get("referer");
+//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+//   if (isWhitelisted) {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "X-Requested-With,Content-Type,Authorization"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//   }
+//   // Pass to next layer of middleware
+//   if (req.method === "OPTIONS") res.sendStatus(200);
+//   else next();
+// });
+app.get("/", (req, res) => {
+  res.send("index.html", { root: path.join(__dirname, "public") });
 });
-app.use(express.static(path.join(__dirname, "public")));
+
 app.get("*", (req, res) => {
   res.send("404");
 });
@@ -58,3 +61,4 @@ app.get("*", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
+module.exports = app;
