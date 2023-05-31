08cdfeadd86a7bd4a8cb9d4d94f9c8e445d04969
Dangeagle <tenanhladang1506@gmail.com>
2023-05-31 11:32:30 +0700

    config to deploy v4

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ var dbUrl = config.dbUrl;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+pp.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
 const connectDB = async () => {
   try {
     await mongoose.connect(dbUrl, {
@@ -28,28 +28,7 @@ connectDB();
 
 app.use("/authen", authenRouter);
 app.use("/api", carzRouter);
-// app.use(compression()); // Compress all routes
-// const whitelist = ["*"];
 
-// app.use((req, res, next) => {
-//   const origin = req.get("referer");
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "X-Requested-With,Content-Type,Authorization"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === "OPTIONS") res.sendStatus(200);
-//   else next();
-// });
 app.get("/", (req, res) => {
   res.send("index.html", { root: path.join(__dirname, "public") });
 });
